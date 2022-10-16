ede7ebf16aeae9112cb934bd57efd6649cd3c53f
Nathan Cruz <113571118+crzn24@users.noreply.github.com>
2022-10-16 15:28:13 -0700

    Attempts to have end screen show when timer ends in js

--- assets/js/script.js
+++ assets/js/script.js
@@ -16,7 +16,7 @@ var timerEl = document.getElementById('countdown');
 
 // Countdown timer funtion
 function countdown() {
-    var timeLeft = 75;
+    var timeLeft = 5;
     var timeInterval = setInterval(function () {
         timeLeft--;
         timerEl.textContent = "Time: " + timeLeft;
@@ -24,11 +24,28 @@ function countdown() {
         if (timeLeft === 0) {
             clearInterval(timeInterval)
             displayMessage();
-
-            timerEl.textContent = "";
         }
     }, 1000);
 }
+
+
+
+
+// End screen function
+function displayMessage() {
+    var endscreen = document.getElementById("endscreen");
+    endscreen.style.display = "block";
+}
+
+// if (endscreen.style.display === "none") {
+//     endscreen.style.display = "block";
+// } else {
+//     endscreen.style.display = "none";
+// }
+
+
+
+
 
 
 
