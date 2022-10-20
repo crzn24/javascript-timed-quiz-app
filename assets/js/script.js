d980115ad84bd152238b46be7807b5bd55211b6a
Nathan Cruz <113571118+crzn24@users.noreply.github.com>
2022-10-19 19:18:03 -0700

    Creates a function to listen for clicks in container of question 1 for correct or incorrect answers

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,8 @@
 var startBtn = document.querySelector("#startquiz");
 var timerEl = document.getElementById('countdown');
 var score = document.querySelector("#finalscore");
+var questionContainer1 = document.querySelector(".question-container1")
+
 
 ///////// State variables /////////////////
 
@@ -16,6 +18,8 @@ var q5 = document.getElementById("question5");
 // Question 1 Buttons
 var q1yes = document.getElementById("q1correct");
 var q1no = document.getElementById("q1incorrect");
+
+
 
 // Countdown timer funtion
 function countdown() {
@@ -40,22 +44,42 @@ function countdown() {
 ////////////// Start Quiz function //////////////
 // When clicking start quiz button, calls countdown function, makes welcome screen disappear
 startquiz.addEventListener("click", function () {
-    countdown();
     var welcome = document.getElementById("welcome");
     welcome.style.display = "none";
-
-
-    
+    countdown();
     q1.style.display = "block";
-    if (q1yes.addEventListener("click", function () {
-        document.getElementById("status").innerHTML = "Correct!";
-    } 
-    ));
-    
-
-
 })
 
+
+
+
+
+///////// Display question funtion ///////////////
+
+
+
+
+// Listens for clicks in question container
+questionContainer1.addEventListener("click", function(event) {
+    var element = event.target;
+  
+    // Check if the clicked element was a button
+    if (element.matches("button")) {
+      // Get the current value of the image's data-state attribute
+      var status = element.getAttribute("data-status");
+  
+      if (status === "correct") {
+        // There are two different ways this attribute can be set
+        element.dataset.state = "animate";
+        element.setAttribute("data-state", "animate");
+        document.getElementById("status").innerHTML = "Correct!";
+  
+      } else {
+        element.dataset.status = "incorrect";
+        document.getElementById("status").innerHTML = "Incorrect!";
+      }
+    }
+  });
 
 
 
